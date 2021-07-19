82244b4914a846b215689cc45e0fa91168d5209a
Tarun Kumar <68737903+tarun0711@users.noreply.github.com>
2021-07-19 20:15:51 +0530

    enhanced

--- THA-18/chess/src/board.jsx
+++ THA-18/chess/src/board.jsx
@@ -1,28 +1,91 @@
-import Box from "./box";
-import "./style.css"
+// import Box from "./box";
+// import "./style.css"
 
-const Board = ()=>{
-    return (
-        <div className="board">
-            {
-            [...Array(64)].map((element,index)=>{
-                let COLOR="indigo";
-                if((Math.floor(index/8)%2===0 && index%2===0) || (Math.floor(index/8)%2!==0 && index%2!==0))
-                {
-                    COLOR="black";
-                }
-                else
-                {
-                    COLOR="white"
-                }
+// const Board = ()=>{
+//     return (
+//         <div className="board">
+//             {
+//             [...Array(64)].map((element,index)=>{
+//                 let COLOR="blue";
+//                 if((Math.floor(index/8)%2===0 && index%2===0) || (Math.floor(index/8)%2!==0 && index%2!==0))
+//                 {
+//                     COLOR="black";
+//                 }
+//                 else
+//                 {
+//                     COLOR="white"
+//                 }
 
-                return(
-                <Box color={COLOR}/>
-                )
-            })
-            }
-        </div>
-    )
-}
+//                 return(
+//                 <Box color={COLOR}/>
+//                 )
+//             })
+//             }
+//         </div>
+//     )
+// }
 
-export default Board;
+const BoxW = () => {
+    return(<div className="boxW"></div>);
+ };
+ 
+ const BoxB = () => {
+     return(<div className="boxB"></div>);
+ };
+ 
+ const RowO = () => {
+     let arr = [];
+     for(let i=0;i<8;i++)
+     {
+         if(i%2===0){
+             arr.push(<BoxW/>);
+         }
+         else{
+             arr.push(<BoxB/>);
+         }
+     }
+ 
+     return(
+         <div className="rowO">
+             {arr}
+         </div>
+     );
+ };
+ 
+ const RowE = () => {
+     let arr1 = [];
+     for(let i=0;i<8;i++)
+     {
+         if(i%2===0){
+             arr1.push(<BoxB/>);
+         }
+         else{
+             arr1.push(<BoxW/>);
+         }
+     }
+ 
+     return(
+         <div className="rowE">
+             {arr1}
+         </div>
+     );
+ };
+ 
+ const Board = () => {
+     let row = []
+     for(let i=0;i<8;i++)
+     {
+         if(i%2===0){
+             row.push(<RowE/>);
+         }
+         else{
+             row.push(<RowO/>);
+         }
+     }
+ 
+     return(
+         <div className="board">{row}</div>
+     );
+ };
+ 
+ export default Board;
--- THA-18/chess/src/index.js
+++ THA-18/chess/src/index.js
@@ -3,8 +3,17 @@ import ReactDOM from "react-dom";
 import Board from "./board";
 import "./style.css";
 
+// const App = () => {
+//   return <Board />;
+// };
+
 const App = () => {
-  return <Board />;
-};
+  return(
+      <div className="container">
+      {/* <h1>Chess Board</h1> */}
+          <Board/>
+      </div>
+  )
+} 
 
 ReactDOM.render(<App />, document.getElementById("root"));
