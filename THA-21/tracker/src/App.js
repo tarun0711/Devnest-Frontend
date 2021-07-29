e95226a3d7d3c79eb8fe92d1a973fd95d33e1ffb
Tarun Kumar <68737903+tarun0711@users.noreply.github.com>
2021-07-29 16:49:16 +0530

    26

--- THA-21/tracker/src/App.js
+++ THA-21/tracker/src/App.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import "./style.css"
 
 function AddItemForm({ addItem }) {
 	const [title, setTitle] = useState('');
--- THA-21/tracker/src/index.js
+++ THA-21/tracker/src/index.js
@@ -1,13 +1,6 @@
-import React from 'react';
-import { StrictMode } from "react";
-import ReactDOM from "react-dom";
-import './style.css';
-import App from "./App";
+import React from 'react'
+import ReactDOM from "react-dom"
+import App from "./App"
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-  rootElement
-);
+
+ReactDOM.render(<App />,document.getElementById('root'));
